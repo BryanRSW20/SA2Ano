21e47b5337691658c552d6b930055e8ba7853c3c
BryanRSW20 <87527079+BryanRSW20@users.noreply.github.com>
2021-08-05 15:40:19 -0300

    Atualização e Adição de Arquivos

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
+        const CalculoQ = function () {
+
+    let valorU = parseFloat(document.getElementById('valorU').value)
+    let valorT = parseFloat(document.getElementById('valorT').value)
+
+    let resultadoQ = valorU+valorT
+    console.log (valorU);
+    console.log(valorT);
+    console.log(resultadoQ);
+
+    document.getElementById('resultadoQ').innerHTML = "A quantidade de calor é " +resultadoQ.toFixed(2)
+}
+        
 
         const CalculoRendimento = function() {
     
-    let Qq = parseFloat(document.getElementById('ValorQq').value)
-    let Qf = parseFloat(document.getElementById('ValorQf').value)
+    let Qq = parseFloat(document.getElementById('valorQq').value)
+    let Qf = parseFloat(document.getElementById('valorQf').value)
     let resultado = (Qq-Qf)/Qq
+    console.log(Qq);
+    console.log(Qf);
     console.log(resultado)
-    document.getElementById("resultado").innerHTML = "O resultado é " +resultado
-}
+    document.getElementById('resultado').innerHTML = "O resultado é " +resultado.toFixed(2) + "%"
+}    
